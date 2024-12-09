ff32455c792f3679d01e02a8eb68fdbaf4f3c897
aha5811 <aha5811@users.noreply.github.com>
2024-12-09 15:13:12 +0100

    day 9.2 solved for text, not for input

--- aha-aoc2024/src/aha/aoc2024/day09/Part1.java
+++ aha-aoc2024/src/aha/aoc2024/day09/Part1.java
@@ -9,46 +9,51 @@ import aha.aoc2024.Part;
 import aha.aoc2024.Utils;
 
 public class Part1 extends Part {
-
+	
 	@Override
 	public Part compute(final String file) {
 		final String line = Utils.readLines(this.dir + file).get(0);
-		
+
 		final int[] fs = read(line);
 		int end = fs.length - 1;
+		
+		end = compact(fs, end);
 
-		int spacePos = 0;
-		while (fs[spacePos] != -1)
-			spacePos++;
-		
-		while (true) {
-			while (fs[end] == -1)
-				end--;
-			
-			final int i = fs[end];
-			end--;
-			
-			fs[spacePos] = i;
-			
-			while (spacePos < end && fs[spacePos] != -1)
-				spacePos++;
-			if (spacePos == end)
-				break;
-		}
-		
 		for (int p = 0; p <= end; p++) {
 			final int v = fs[p];
 			if (v == -1)
 				continue;
 			this.res += p * v;
 		}
-
+		
 		return this;
 	}
+	
+	protected int compact(final int[] fs, int end) {
+		int spacePos = 0;
+		while (fs[spacePos] != -1)
+			spacePos++;
 
+		while (true) {
+			while (fs[end] == -1)
+				end--;
+
+			final int i = fs[end];
+			end--;
+
+			fs[spacePos] = i;
+
+			while (spacePos < end && fs[spacePos] != -1)
+				spacePos++;
+			if (spacePos == end)
+				break;
+		}
+		return end;
+	}
+	
 	private int[] read(final String line) {
 		final List<Integer> fs = new LinkedList<>();
-		
+
 		int id = 0;
 		boolean isFile = true;
 		for (final char c : line.toCharArray()) {
@@ -60,28 +65,28 @@ public class Part1 extends Part {
 				add(fs, i, -1);
 			isFile = !isFile;
 		}
-
+		
 		final int[] fss = new int[fs.size()];
-
+		
 		int i = 0;
 		while (!fs.isEmpty())
 			fss[i++] = fs.remove(0);
 		return fss;
 	}
-	
+
 	private void add(final List<Integer> fs, final int length, final int id) {
 		for (int i = 0; i < length; i++)
 			fs.add(id);
 	}
-	
+
 	@Override
 	public void aTest() {
 		assertEquals(1928, new Part1().compute("test.txt").res);
 	}
-
+	
 	@Override
 	public void main() {
 		assertEquals(6283170117911l, new Part1().compute("input.txt").res);
 	}
-	
+
 }
--- aha-aoc2024/src/aha/aoc2024/day09/Part2.java
+++ aha-aoc2024/src/aha/aoc2024/day09/Part2.java
@@ -2,8 +2,72 @@ package aha.aoc2024.day09;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Part2 extends Part1 {
 
+	@Override
+	protected int compact(final int[] fs, final int end) {
+		final List<Block> blocks = new LinkedList<>();
+
+		int start = -1;
+		int id = -2;
+		
+		for (int p = 0; p < end; p++)
+			if (fs[p] != id) {
+				if (start != -1)
+					blocks.add(new Block(start, p - start, id));
+				start = p;
+				id = fs[p];
+			}
+		if (id != -1)
+			blocks.add(new Block(start, end - start + 1, id));
+
+		final List<Block> files = new LinkedList<>(), spaces = new LinkedList<>();
+		
+		for (final Block b : blocks)
+			if (b.id == -1)
+				spaces.add(b);
+			else
+				files.add(b);
+
+		Collections.reverse(files);
+
+		for (final Block f : files)
+			move:
+				for (final Block space : spaces)
+					if (space.l >= f.l) {
+						for (int i = 0; i < f.l; i++) {
+							fs[space.p + i] = f.id;
+							fs[f.p + i] = -1;
+						}
+						space.p += f.l;
+						space.l -= f.l;
+						break move;
+					}
+		
+		return end;
+	}
+
+	static class Block {
+		int p;
+		int l;
+		int id;
+
+		public Block(final int p, final int l, final int id) {
+			this.p = p;
+			this.l = l;
+			this.id = id;
+		}
+
+		@Override
+		public String toString() {
+			return (this.id == -1 ? "X" : this.id) + ":" + this.p + "/" + this.l;
+		}
+	}
+	
 	@Override
 	public void aTest() {
 		assertEquals(2858, new Part2().compute("test.txt").res);
@@ -11,7 +75,8 @@ public class Part2 extends Part1 {
 	
 	@Override
 	public void main() {
-		// assertEquals(0, new Part2().compute("input.txt").res);
+		assertEquals(0, new Part2().compute("input.txt").res);
+		// 8449767616409l is too high
 	}
 	
 }
