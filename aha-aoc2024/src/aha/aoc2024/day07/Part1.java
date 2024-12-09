719d82b2e4f793e4539edce8d81c7e222a4b6876
aha5811 <aha5811@users.noreply.github.com>
2024-12-09 11:11:57 +0100

    day 7.2

--- aha-aoc2024/src/aha/aoc2024/day07/Part1.java
+++ aha-aoc2024/src/aha/aoc2024/day07/Part1.java
@@ -8,44 +8,67 @@ import aha.aoc2024.Part;
 import aha.aoc2024.Utils;
 
 public class Part1 extends Part {
-
+	
 	@Override
 	public Part compute(final String file) {
 		for (final String line : Utils.readLines(this.dir + file)) {
 			final List<Long> ls = Utils.toLs(line.replace(":", ""));
 			final Long target = ls.remove(0);
-			if (solvable(target, ls))
+			if (solvable(target, ls, getFs()))
 				this.res += target;
 		}
 		return this;
 	}
 
-	private boolean solvable(final long res, final List<Long> ls) {
-		return solvableRec(res, ls.remove(0), ls);
+	static interface F {
+		public long f(long l1, long l2);
 	}
 	
-	boolean solvableRec(final long target, final long tmp, final List<Long> ls) {
-		
+	static F Plus = new F() {
+		@Override
+		public long f(final long l1, final long l2) {
+			return l1 + l2;
+		};
+	};
+
+	static F Mult = new F() {
+		@Override
+		public long f(final long l1, final long l2) {
+			return l1 * l2;
+		};
+	};
+
+	protected F[] getFs() { return new F[] { Plus, Mult }; }
+	
+	private boolean solvable(final long res, final List<Long> ls, final F[] fs) {
+		return solvableRec(res, ls.remove(0), ls, fs);
+	}
+	
+	private final boolean solvableRec(final long target, final long tmp, final List<Long> ls, final F[] fs) {
+
 		if (ls.isEmpty())
 			return target == tmp;
-
+		
 		if (tmp > target)
 			return false;
-
+		
 		final long l = ls.get(0);
 		final List<Long> lsNext = ls.subList(1, ls.size());
 		
-		return solvableRec(target, tmp + l, lsNext) || solvableRec(target, tmp * l, lsNext);
+		boolean ret = false;
+		for (final F f : fs)
+			ret |= solvableRec(target, f.f(tmp, l), lsNext, fs);
+		return ret;
 	}
-	
+
 	@Override
 	public void aTest() {
 		assertEquals(3749, new Part1().compute("test.txt").res);
 	}
-
+	
 	@Override
 	public void main() {
 		assertEquals(1399219271639l, new Part1().compute("input.txt").res);
 	}
-	
+
 }
--- aha-aoc2024/src/aha/aoc2024/day07/Part2.java
+++ aha-aoc2024/src/aha/aoc2024/day07/Part2.java
@@ -3,15 +3,25 @@ package aha.aoc2024.day07;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Part2 extends Part1 {
-
+	
+	static F Conc = new F() {
+		@Override
+		public long f(final long l1, final long l2) {
+			return Long.parseLong(Long.toString(l1) + Long.toString(l2));
+		}
+	};
+	
+	@Override
+	protected F[] getFs() { return new F[] { Plus, Mult, Conc }; }
+	
 	@Override
 	public void aTest() {
-		assertEquals(0, new Part2().compute("test.txt").res);
+		assertEquals(11387, new Part2().compute("test.txt").res);
 	}
-	
+
 	@Override
 	public void main() {
-		// assertEquals(0, new Part2().compute("input.txt").res);
+		assertEquals(275791737999003l, new Part2().compute("input.txt").res);
 	}
-	
+
 }
