8f073a8fa273e23b11a334f4c653ec089008d6cf
aha5811 <aha5811@users.noreply.github.com>
2024-12-09 11:58:48 +0100

    day 8.2

--- aha-aoc2024/src/aha/aoc2024/day08/Part2.java
+++ aha-aoc2024/src/aha/aoc2024/day08/Part2.java
@@ -2,8 +2,31 @@ package aha.aoc2024.day08;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import aha.aoc2024.Utils.CharMap;
+import aha.aoc2024.Utils.Pos;
+import aha.aoc2024.Utils.Symbol;
+
 public class Part2 extends Part1 {
 
+	@Override
+	protected List<Pos> getAntinodes(final CharMap cm, final Symbol a1, final Symbol a2) {
+		final List<Pos> ret = new LinkedList<>();
+
+		int dist = 0; // so they are all also antinodes!
+		while (true) {
+			final Pos p = getP(a1, a2, dist);
+			if (cm.isOutside(p.x, p.y))
+				break;
+			ret.add(p);
+			dist++;
+		}
+
+		return ret;
+	}
+	
 	@Override
 	public void aTest() {
 		assertEquals(34, new Part2().compute("test.txt").res);
@@ -11,7 +34,7 @@ public class Part2 extends Part1 {
 	
 	@Override
 	public void main() {
-		// assertEquals(0, new Part2().compute("input.txt").res);
+		assertEquals(927, new Part2().compute("input.txt").res);
 	}
 	
 }
