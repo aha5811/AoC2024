d26dd28d444152dabd215726a9d1e175202cd25e
aha5811 <aha5811@users.noreply.github.com>
2024-12-09 15:35:23 +0100

    part 6.2 still not solved
    simplifies part1

--- aha-aoc2024/src/aha/aoc2024/day09/Part1.java
+++ aha-aoc2024/src/aha/aoc2024/day09/Part1.java
@@ -9,71 +9,77 @@ import aha.aoc2024.Part;
 import aha.aoc2024.Utils;
 
 public class Part1 extends Part {
-	
+
 	@Override
 	public Part compute(final String file) {
-		final String line = Utils.readLines(this.dir + file).get(0);
-
-		final int[] fs = read(line);
-		int end = fs.length - 1;
 		
-		end = compact(fs, end);
-
-		for (int p = 0; p <= end; p++) {
+		final int[] fs = read(Utils.readLines(this.dir + file).get(0));
+		
+		compact(fs);
+		
+		for (int p = 0; p < fs.length; p++) {
 			final int v = fs[p];
 			if (v == -1)
 				continue;
 			this.res += p * v;
 		}
-		
+
 		return this;
 	}
 	
-	protected int compact(final int[] fs, int end) {
+	protected void compact(final int[] fs) {
+		
 		int spacePos = 0;
 		while (fs[spacePos] != -1)
 			spacePos++;
+		
+		int mv = fs.length - 1;
 
 		while (true) {
-			while (fs[end] == -1)
-				end--;
-
-			final int i = fs[end];
-			end--;
-
+			while (fs[mv] == -1)
+				mv--;
+			
+			final int i = fs[mv];
+			fs[mv] = -1;
+			mv--;
+			
 			fs[spacePos] = i;
-
-			while (spacePos < end && fs[spacePos] != -1)
+			
+			while (spacePos < mv && fs[spacePos] != -1)
 				spacePos++;
-			if (spacePos == end)
+			if (spacePos == mv)
 				break;
 		}
-		return end;
+		
 	}
-	
-	private int[] read(final String line) {
-		final List<Integer> fs = new LinkedList<>();
 
-		int id = 0;
-		boolean isFile = true;
-		for (final char c : line.toCharArray()) {
-			final int i = Integer.parseInt("" + c);
-			if (isFile) {
-				add(fs, i, id);
-				id++;
-			} else
-				add(fs, i, -1);
-			isFile = !isFile;
+	private int[] read(final String line) {
+		
+		final List<Integer> fs = new LinkedList<>();
+		{
+			int id = 0;
+			boolean isFile = true;
+			for (final char c : line.toCharArray()) {
+				final int i = Integer.parseInt("" + c);
+				if (isFile) {
+					add(fs, i, id);
+					id++;
+				} else
+					add(fs, i, -1);
+				isFile = !isFile;
+			}
 		}
-		
+
 		final int[] fss = new int[fs.size()];
-		
-		int i = 0;
-		while (!fs.isEmpty())
-			fss[i++] = fs.remove(0);
+		{
+			int i = 0;
+			while (!fs.isEmpty())
+				fss[i++] = fs.remove(0);
+		}
+
 		return fss;
 	}
-
+	
 	private void add(final List<Integer> fs, final int length, final int id) {
 		for (int i = 0; i < length; i++)
 			fs.add(id);
@@ -83,10 +89,10 @@ public class Part1 extends Part {
 	public void aTest() {
 		assertEquals(1928, new Part1().compute("test.txt").res);
 	}
-	
+
 	@Override
 	public void main() {
 		assertEquals(6283170117911l, new Part1().compute("input.txt").res);
 	}
-
+	
 }
--- aha-aoc2024/src/aha/aoc2024/day09/Part2.java
+++ aha-aoc2024/src/aha/aoc2024/day09/Part2.java
@@ -7,34 +7,39 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Part2 extends Part1 {
-
+	
 	@Override
-	protected int compact(final int[] fs, final int end) {
+	protected void compact(final int[] fs) {
+
 		final List<Block> blocks = new LinkedList<>();
+		{
+			int start = -1;
+			int id = -2;
 
-		int start = -1;
-		int id = -2;
+			for (int p = 0; p < fs.length; p++)
+				if (fs[p] != id) {
+					if (start != -1)
+						blocks.add(new Block(start, p - start, id));
+					start = p;
+					id = fs[p];
+				}
+			if (id != -1)
+				blocks.add(new Block(start, fs.length - start, id));
+		}
 		
-		for (int p = 0; p < end; p++)
-			if (fs[p] != id) {
-				if (start != -1)
-					blocks.add(new Block(start, p - start, id));
-				start = p;
-				id = fs[p];
-			}
-		if (id != -1)
-			blocks.add(new Block(start, end - start + 1, id));
-
-		final List<Block> files = new LinkedList<>(), spaces = new LinkedList<>();
+		final List<Block>
+		files = new LinkedList<>(),
+		spaces = new LinkedList<>();
+		{
+			for (final Block b : blocks)
+				if (b.id == -1)
+					spaces.add(b);
+				else
+					files.add(b);
+			files.remove(0);
+			Collections.reverse(files);
+		}
 		
-		for (final Block b : blocks)
-			if (b.id == -1)
-				spaces.add(b);
-			else
-				files.add(b);
-
-		Collections.reverse(files);
-
 		for (final Block f : files)
 			move:
 				for (final Block space : spaces)
@@ -48,35 +53,42 @@ public class Part2 extends Part1 {
 						break move;
 					}
 		
-		return end;
 	}
 
+	private void out(final int[] fs) {
+		String out = "";
+		for (final int element : fs)
+			out += "|" + (element == -1 ? "." : element);
+		System.out.println(out);
+	}
+	
 	static class Block {
 		int p;
 		int l;
 		int id;
-
+		
 		public Block(final int p, final int l, final int id) {
 			this.p = p;
 			this.l = l;
 			this.id = id;
 		}
-
+		
 		@Override
 		public String toString() {
 			return (this.id == -1 ? "X" : this.id) + ":" + this.p + "/" + this.l;
 		}
 	}
-	
+
 	@Override
 	public void aTest() {
 		assertEquals(2858, new Part2().compute("test.txt").res);
 	}
-	
+
 	@Override
 	public void main() {
 		assertEquals(0, new Part2().compute("input.txt").res);
 		// 8449767616409l is too high
+		// 8450064123278l is too high
 	}
-	
+
 }
