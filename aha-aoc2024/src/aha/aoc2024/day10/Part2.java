f9217983f32c4ffd0ee45ae6ea126a04017b09ed
aha5811 <aha5811@users.noreply.github.com>
2024-12-10 09:57:47 +0100

    day 10 refactoring

--- aha-aoc2024/src/aha/aoc2024/day10/Part2.java
+++ aha-aoc2024/src/aha/aoc2024/day10/Part2.java
@@ -5,36 +5,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import aha.aoc2024.Utils;
 
 public class Part2 extends Part1 {
-
+	
 	@Override
-	protected void computeRating(final DigiMap dm, final int x, final int y) {
-		this.res += getTHC(dm, x, y);
+	protected int getTHscore(final DigiMap dm, final int x, final int y) {
+		return getWays(dm, x, y);
 	}
-
-	private int getTHC(final DigiMap dm, final int x, final int y) {
-
+	
+	private int getWays(final DigiMap dm, final int x, final int y) {
+		
 		final int here = dm.getInt(x, y);
-
+		
 		if (here == 9)
 			return 1;
-
+		
 		int ret = 0;
 		for (final int[] d : Utils.DIRS90) {
 			final int xd = x + d[0], yd = y + d[1];
 			if (!dm.isOutside(xd, yd) && dm.getInt(xd, yd) == here + 1)
-				ret += getTHC(dm, xd, yd);
+				ret += getWays(dm, xd, yd);
 		}
 		return ret;
 	}
-	
+
 	@Override
 	public void aTest() {
 		assertEquals(81, new Part2().compute("test.txt").res);
 	}
-	
+
 	@Override
 	public void main() {
 		assertEquals(1925, new Part2().compute("input.txt").res);
 	}
-	
+
 }
