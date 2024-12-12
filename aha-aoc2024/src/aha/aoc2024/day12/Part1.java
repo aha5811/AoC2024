809293b8db1e4e9c8b967d8656e736f0997bfba7
aha5811 <aha5811@users.noreply.github.com>
2024-12-12 15:23:38 +0100

    editorial changes

--- aha-aoc2024/src/aha/aoc2024/day12/Part1.java
+++ aha-aoc2024/src/aha/aoc2024/day12/Part1.java
@@ -15,39 +15,39 @@ import aha.aoc2024.Utils.Pos;
 import aha.aoc2024.Utils.Symbol;
 
 public class Part1 extends Part {
-	
-	// https://adventofcode.com/2024/day/12
 
+	// https://adventofcode.com/2024/day/12
+	
 	@Override
 	public Part compute(final String file) {
 		final CharMap cm = new CharMap(this.dir + file);
-
-		final List<Region> regions = getRegions(cm, getRegFac());
 		
+		final List<Region> regions = getRegions(cm, getRegFac());
+
 		for (final Region r : regions)
 			this.res += getFenceCost(r);
-		
+
 		return this;
 	}
-
+	
 	interface RegFac {
 		Region from(final Symbol s);
 	}
-
-	protected RegFac getRegFac() { return s -> new Region(s); }
 	
+	protected RegFac getRegFac() { return s -> new Region(s); }
+
 	protected int getFenceCost(final Region r) {
 		final int b = r.getBorder(), s = r.size(), total = s * b;
-		System.out.println(r.c + " " + s + " * " + b + " = " + total);
+		// System.out.println(r.c + " " + s + " * " + b + " = " + total);
 		return total;
 	}
-	
+
 	private List<Region> getRegions(final CharMap cm, final RegFac rf) {
 		final List<Region> ret = new LinkedList<>();
-
+		
 		for (final char c : cm.getChars()) {
 			List<Region> tmp = new LinkedList<>();
-			
+
 			for (final Symbol s : cm.getAll(c)) {
 				boolean found = false;
 				for (final Region r : tmp)
@@ -58,7 +58,7 @@ public class Part1 extends Part {
 				if (!found)
 					tmp.add(rf.from(s));
 			}
-			
+
 			// merge
 			while (tmp.size() > 1) {
 				final List<Region> merged = new LinkedList<>();
@@ -77,22 +77,22 @@ public class Part1 extends Part {
 				if (tmp.size() == inSize)
 					break;
 			}
-			
+
 			ret.addAll(tmp);
 		}
-
+		
 		return ret;
 	}
-
+	
 	static class Region {
 		char c;
 		Collection<Symbol> ss = new HashSet<>();
-
+		
 		public Region(final Symbol s) {
 			this.c = s.c;
 			add(s);
 		}
-		
+
 		boolean merges(final Region other) {
 			boolean touches = false;
 			for (final Symbol s : other.ss)
@@ -104,35 +104,35 @@ public class Part1 extends Part {
 				this.ss.addAll(other.ss);
 			return touches;
 		}
-
+		
 		void add(final Symbol s) {
 			this.ss.add(s);
 		}
-		
+
 		boolean isNeighbour(final Symbol s) {
 			if (s.c != this.c)
 				return false;
 			return isNeighbourP(s);
 		}
-		
+
 		boolean isNeighbourP(final Pos p) {
 			for (final Pos ap : this.ss)
 				if (isNeighbour(p, ap))
 					return true;
 			return false;
 		}
-		
+
 		int size() {
 			return this.ss.size();
 		}
-
+		
 		int getBorder() {
 			int ret = 0;
 			for (final Pos p : this.ss)
 				ret += 4 - getNeighbours(p);
 			return ret;
 		}
-		
+
 		protected final int getNeighbours(final Pos p) {
 			int ret = 0;
 			for (final Pos ap : this.ss)
@@ -140,26 +140,26 @@ public class Part1 extends Part {
 					ret++;
 			return ret;
 		}
-		
+
 		private boolean isNeighbour(final Pos p1, final Pos p2) {
 			for (final int[] dir : Utils.DIRS90)
 				if (p1.x + dir[0] == p2.x && p1.y + dir[1] == p2.y)
 					return true;
 			return false;
 		}
-
+		
 	}
-
+	
 	@Override
 	public void aTest() {
 		assertEquals(140, new Part1().compute("test1.txt").res);
 		assertEquals(772, new Part1().compute("test2.txt").res);
 		assertEquals(1930, new Part1().compute("test.txt").res);
 	}
-	
+
 	@Override
 	public void main() {
 		assertEquals(1421958, new Part1().compute("input.txt").res);
 	}
-
+	
 }
--- aha-aoc2024/src/aha/aoc2024/day12/Part2.java
+++ aha-aoc2024/src/aha/aoc2024/day12/Part2.java
@@ -10,149 +10,140 @@ import aha.aoc2024.Utils.Pos;
 import aha.aoc2024.Utils.Symbol;
 
 public class Part2 extends Part1 {
-
+	
 	// https://adventofcode.com/2024/day/12#part2
-
+	
 	@Override
 	protected RegFac getRegFac() { return s -> new Region2(s); }
-
+	
 	@Override
 	protected int getFenceCost(final Region r) {
 		final int b = ((Region2) r).getSides(), s = r.size(), total = s * b;
-		System.out.println(r.c + " " + s + " * " + b + " = " + total);
+		// System.out.println(r.c + " " + s + " * " + b + " = " + total);
 		return total;
 	}
 
-	static class Region2 extends Region {
+	static class OutPos extends Pos {
+		int[] dir;
+		
+		public OutPos(final int x, final int y, final int[] dir) {
+			super(x, y);
+			this.dir = dir;
+		}
+		
+		@Override
+		public String toString() {
+			return super.toString() + toString(this.dir);
+		}
 
+		private String toString(final int[] dir) {
+			if (dir == Utils.U)
+				return "^";
+			else if (dir == Utils.R)
+				return ">";
+			else if (dir == Utils.D)
+				return "v";
+			else if (dir == Utils.L)
+				return "<";
+			return "?";
+		}
+
+		@Override
+		public boolean equals(final Object obj) {
+			if (obj == null || !(obj instanceof OutPos))
+				return false;
+			if (obj == this)
+				return true;
+			final OutPos other = (OutPos) obj;
+			return other.x == this.x && other.y == this.y && other.dir == this.dir;
+		}
+	}
+
+	static class Region2 extends Region {
+		
 		public Region2(final Symbol s) {
 			super(s);
 		}
-		
-		static class OutPos extends Pos {
-			int[] dir;
 
-			public OutPos(final int x, final int y, final int[] dir) {
-				super(x, y);
-				this.dir = dir;
-			}
-
-			@Override
-			public String toString() {
-				return super.toString() + toString(this.dir);
-			}
-			
-			private String toString(final int[] dir) {
-				if (dir == Utils.U)
-					return "^";
-				else if (dir == Utils.R)
-					return ">";
-				else if (dir == Utils.D)
-					return "v";
-				else if (dir == Utils.L)
-					return "<";
-				return "?";
-			}
-			
-			@Override
-			public boolean equals(final Object obj) {
-				if (obj == null || !(obj instanceof OutPos))
-					return false;
-				if (obj == this)
-					return true;
-				final OutPos other = (OutPos) obj;
-				return other.x == this.x && other.y == this.y && other.dir == this.dir;
-			}
-		}
-		
 		int getSides() {
-
+			
 			final Collection<OutPos> ops = new HashSet<>();
 			{
 				final Collection<Pos> ps = new HashSet<>();
 				for (final Pos p : this.ss)
 					ps.add(new Pos(p.x, p.y));
-				
+
 				for (final Pos p : ps)
 					for (final int[] dir : Utils.DIRS90)
 						if (!ps.contains(new Pos(p.x + dir[0], p.y + dir[1])))
 							ops.add(new OutPos(p.x, p.y, dir));
 			}
-			
+
 			int ret = 0;
 			while (!ops.isEmpty())
 				ret += removeRing(ops);
 			return ret;
 		}
-
+		
 		private int removeRing(final Collection<OutPos> ops) {
 			int ret = 0;
-			
-			final OutPos start = ops.iterator().next();
-			ops.remove(start);
 
-			OutPos current = start;
-			int[] dir = turnRight(start.dir);
+			final OutPos start = ops.iterator().next();
 			
+			OutPos curr = start;
+			int[] onward = turnRight(start.dir);
+
 			while (!ops.isEmpty()) {
-				final OutPos nextOn = new OutPos(current.x + dir[0], current.y + dir[1], current.dir),
-						nextLeft = new OutPos(
-								current.x + dir[0] + current.dir[0], current.y + dir[1]
-										+ current.dir[1], turnLeft(current.dir)
-								), nextRight = new OutPos(current.x, current.y, dir);
+				ops.remove(curr);
 				
-				if (nextOn.equals(start)) {
-					ops.remove(current);
+				final OutPos
+				nextOn = new OutPos(curr.x + onward[0], curr.y + onward[1], curr.dir),
+				nextLeft =
+				new OutPos(
+						curr.x + onward[0] + curr.dir[0],
+						curr.y + onward[1] + curr.dir[1],
+						turnLeft(curr.dir)),
+				nextRight = new OutPos(curr.x, curr.y, onward);
+
+				if (nextOn.equals(start))
 					break;
-				} else if (nextLeft.equals(start) || nextRight.equals(start)) {
-					ops.remove(current);
+				else if (nextLeft.equals(start) || nextRight.equals(start)) {
 					ret++;
 					break;
-				} else if (ops.contains(nextOn)) {
-					ops.remove(current);
-					current = nextOn;
-				} else if (ops.contains(nextLeft)) {
-					ops.remove(current);
-					current = nextLeft;
-					dir = turnLeft(dir);
+				} else if (ops.contains(nextOn))
+					curr = nextOn;
+				else if (ops.contains(nextLeft)) {
+					curr = nextLeft;
+					onward = turnLeft(onward);
 					ret++;
 				} else if (ops.contains(nextRight)) {
-					ops.remove(current);
-					current = nextRight;
-					dir = turnRight(dir);
+					curr = nextRight;
+					onward = turnRight(onward);
 					ret++;
 				}
 			}
-			
+
 			return ret;
 		}
-
-	}
-	
-	static int[] turnLeft(final int[] dir) {
-		if (dir == Utils.U)
-			return Utils.L;
-		if (dir == Utils.L)
-			return Utils.D;
-		if (dir == Utils.D)
-			return Utils.R;
-		if (dir == Utils.R)
-			return Utils.U;
-		return null;
+		
 	}
 	
 	static int[] turnRight(final int[] dir) {
-		if (dir == Utils.U)
-			return Utils.R;
-		if (dir == Utils.R)
-			return Utils.D;
-		if (dir == Utils.D)
-			return Utils.L;
-		if (dir == Utils.L)
-			return Utils.U;
+		if (dir == Utils.U) return Utils.R;
+		if (dir == Utils.R) return Utils.D;
+		if (dir == Utils.D) return Utils.L;
+		if (dir == Utils.L) return Utils.U;
 		return null;
 	}
-
+	
+	static int[] turnLeft(final int[] dir) {
+		if (dir == Utils.U) return Utils.L;
+		if (dir == Utils.L) return Utils.D;
+		if (dir == Utils.D) return Utils.R;
+		if (dir == Utils.R) return Utils.U;
+		return null;
+	}
+	
 	@Override
 	public void aTest() {
 		assertEquals(80, new Part2().compute("test1.txt").res);
@@ -161,10 +152,10 @@ public class Part2 extends Part1 {
 		assertEquals(368, new Part2().compute("test4.txt").res);
 		assertEquals(1206, new Part2().compute("test.txt").res);
 	}
-	
+
 	@Override
 	public void main() {
-		assertEquals(0, new Part2().compute("input.txt").res);
+		assertEquals(885394, new Part2().compute("input.txt").res);
 	}
-	
+
 }
