802a1742676e24b744594793fd91dfc45bb0900c
aha5811 <aha5811@users.noreply.github.com>
2024-12-12 16:55:45 +0100

    day 11.2

--- aha-aoc2024/src/aha/aoc2024/day11/Part2.java
+++ aha-aoc2024/src/aha/aoc2024/day11/Part2.java
@@ -2,98 +2,62 @@ package aha.aoc2024.day11;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class Part2 extends Part1 {
-
+	
 	// https://adventofcode.com/2024/day/11#part2
+
+	private static <K> void inc(final Map<K, Long> map, final K k, final Long m) {
+		if (!map.containsKey(k))
+			map.put(k, 0l);
+		map.put(k, map.get(k) + m);
+	}
+
+	@Override
+	protected long computeExplosion(final List<Long> startLs) {
+		
+		Map<Long, Long> l2n = new HashMap<>();
+		for (final Long l : startLs)
+			inc(l2n, l, 1l);
+		
+		for (int blink = 0; blink < this.blinks; blink++) {
+			final Map<Long, Long> l2nNext = new HashMap<>();
+			final List<Long> keys = new LinkedList<>(l2n.keySet());
+			for (final Long l : keys) {
+				final long n = l2n.get(l);
+				String ls;
+				if (l == 0)
+					inc(l2nNext, 1l, n);
+				else if ((ls = Long.toString(l)).length() % 2 == 0) {
+					final int split = ls.length() / 2;
+					inc(l2nNext, Long.parseLong(ls.substring(0, split)), n);
+					inc(l2nNext, Long.parseLong(ls.substring(split)), n);
+				} else
+					inc(l2nNext, l * 2024, n);
+			}
+			l2n = l2nNext;
+		}
+		
+		long ret = 0;
+		for (final long l : l2n.keySet())
+			ret += l2n.get(l);
+		return ret;
+		
+	}
 	
 	@Override
-	protected long computeExplosion(final List<Long> ls) {
-
-		// check how many different numbers will emerge
-		{
-			final Collection<Long> all = new HashSet<>();
-			int blink = 0;
-			List<Long> start = ls;
-			while (true) {
-				all.addAll(start);
-				final List<Long> next = explode(start);
-				blink++;
-				next.removeAll(all);
-				if (next.isEmpty())
-					break;
-				start = next;
-			}
-			
-			final List<Long> alls = new LinkedList<>(all);
-			Collections.sort(alls);
-			
-			System.out.println("no more new numbers after blink " + blink);
-			System.out.println(alls.size() + ": " + alls);
-		}
-
-		System.out.println("-------------------------------");
-
-		// check how long it takes for each digit to emerge again
-		// 0 -> 1 -> 2024 -> 20 24 -> 2 0 2 4 => 4 blinks
-		// 1 -> 2024 -> 20 24 -> 2 0 2 4 -> ... 1 ... ... => 4 blinks
-		// 2 -> 4048 -> 40 48 -> 0 -> 1 -> 2024 -> 20 24 -> 2 => 7 blinks
-		// ...
-
-		for (long l = 0; l <= 9; l++) {
-			List<Long> start = new LinkedList<>();
-			start.add(l);
-			List<Long> next;
-			int blink = 0;
-			while (true) {
-				next = explode(start);
-				blink++;
-				if (next.contains(l))
-					break;
-				start = next;
-			}
-			System.out.println(
-					"cycle test for " + l
-					+ " took " + blink + " blinks"
-					+ " to get " + count(next, l) + " '" + l + "'s"
-					+ " in " + next);
-		}
-
-		return 0;
+	public void aTest() {
+		assertEquals(22, new Part2().setBlinks(6).compute("test.txt").res);
+		assertEquals(55312, new Part2().setBlinks(25).compute("test.txt").res);
 	}
-
-	private int count(final List<Long> ls, final long l) {
-		int ret = 0;
-		for (final long al : ls)
-			if (l == al)
-				ret++;
-		return ret;
-	}
-
-	private List<Long> explode(final List<Long> list) {
-		final List<Long> ret = new LinkedList<>();
-		for (final long l : list) {
-			String ls;
-			if (l == 0)
-				ret.add(1l);
-			else if ((ls = Long.toString(l)).length() % 2 == 0) {
-				final int split = ls.length() / 2;
-				ret.add(Long.parseLong(ls.substring(0, split)));
-				ret.add(Long.parseLong(ls.substring(split)));
-			} else
-				ret.add(l * 2024);
-		}
-		return ret;
-	}
-
+	
 	@Override
 	public void main() {
-		assertEquals(1, new Part2().setBlinks(75).compute("input.txt").res);
+		assertEquals(259112729857522l, new Part2().setBlinks(75).compute("input.txt").res);
 	}
-	
+
 }
