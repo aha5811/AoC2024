f3c0e51cdbd1b14311e965c083e08d6bea577635
aha5811 <aha5811@users.noreply.github.com>
2025-01-09 18:08:48 +0100

    day 22

---
+++ aha-aoc2024/src/aha/aoc2024/day22/Part1.java
@@ -0,0 +1,76 @@
+package aha.aoc2024.day22;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+import aha.aoc2024.Part;
+import aha.aoc2024.Utils;
+
+public class Part1 extends Part {
+	
+	// https://adventofcode.com/2024/day/_
+
+	final static int IT = 2000;
+	
+	@Override
+	public Part compute(final String file) {
+		for (final String s : Utils.readLines(this.dir + file))
+			try {
+				final Integer i = Integer.parseInt(s);
+				this.res += getNumber(i, IT);
+			} catch (final Exception ex) {
+			}
+		return this;
+	}
+
+	private long getNumber(final long l, final int it) {
+		long secret = l;
+		for (int i = 0; i < it; i++)
+			secret = getSecret(secret);
+		return secret;
+	}
+
+	protected long getSecret(long secret) {
+		final long l2 = secret * 64,
+				l3 = secret ^ l2,
+				l4 = l3 % 16777216,
+				l5 = l4 / 32,
+				l6 = l4 ^ l5,
+				l7 = l6 % 16777216,
+				l8 = l7 * 2048,
+				l9 = l7 ^ l8,
+				l10 = l9 % 16777216;
+		secret = l10;
+		return secret;
+	}
+
+	@Test
+	public void test() {
+		assertEquals(37, 42 ^ 15);
+		assertEquals(16113920, 100000000 % 16777216);
+		
+		long n = 123;
+		assertEquals(15887950, n = new Part1().getNumber(n, 1));
+		assertEquals(16495136, n = new Part1().getNumber(n, 1));
+		assertEquals(527345, n = new Part1().getNumber(n, 1));
+		assertEquals(704524, n = new Part1().getNumber(n, 1));
+		assertEquals(1553684, n = new Part1().getNumber(n, 1));
+		assertEquals(12683156, n = new Part1().getNumber(n, 1));
+		assertEquals(11100544, n = new Part1().getNumber(n, 1));
+		assertEquals(12249484, n = new Part1().getNumber(n, 1));
+		assertEquals(7753432, n = new Part1().getNumber(n, 1));
+		assertEquals(5908254, n = new Part1().getNumber(n, 1));
+	}
+	
+	@Override
+	public void aTest() {
+		assertEquals(37327623, new Part1().compute("test.txt").res);
+	}
+	
+	@Override
+	public void main() {
+		assertEquals(18261820068l, new Part1().compute("input.txt").res);
+	}
+
+}
---
+++ aha-aoc2024/src/aha/aoc2024/day22/Part2.java
@@ -0,0 +1,97 @@
+package aha.aoc2024.day22;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import aha.aoc2024.Part;
+import aha.aoc2024.Utils;
+
+public class Part2 extends Part1 {
+
+	// https://adventofcode.com/2024/day/_#part2
+
+	private int it = -1;
+	
+	public Part2 setIt(final int it) {
+		this.it = it;
+		return this;
+	}
+	
+	@Override
+	public Part compute(final String file) {
+
+		final Map<Integer, Map<String, Integer>> b2s2n = new HashMap<>();
+		final Collection<String> seqs = new HashSet<>();
+
+		final int theIt = this.it == -1 ? IT : this.it;
+
+		{
+			int n = 0;
+			for (final String s : Utils.readLines(this.dir + file)) {
+				final List<Integer> changes = new LinkedList<>();
+				long secret = Integer.parseInt(s);
+				for (int i = 0; i < theIt; i++) {
+					final long next = getSecret(secret);
+					final int sd = lastDigit(secret);
+					final int nd = lastDigit(next);
+					final int change = nd - sd;
+					changes.add(0, change);
+					
+					// System.out.println(next + ": " + nd + " (" + change + ")");
+					
+					if (changes.size() >= 4) {
+						final String seq = changes.get(3) + "," + changes.get(2) + "," + changes.get(1) + ","
+								+ changes.get(0);
+						seqs.add(seq);
+						if (!b2s2n.containsKey(n))
+							b2s2n.put(n, new HashMap<>());
+						if (!b2s2n.get(n).containsKey(seq))
+							b2s2n.get(n).put(seq, nd);
+					}
+					secret = next;
+				}
+				n++;
+			}
+		}
+
+		long bananas = 0;
+		String winningSeq = null;
+		for (final String seq : seqs) {
+			long sbs = 0;
+			for (final Integer n : b2s2n.keySet())
+				if (b2s2n.get(n).containsKey(seq))
+					sbs += b2s2n.get(n).get(seq);
+			if (sbs > bananas) {
+				bananas = sbs;
+				winningSeq = seq;
+			}
+		}
+
+		// System.out.println(winningSeq + " -> " + bananas);
+		this.res = bananas;
+		
+		return this;
+	}
+
+	private int lastDigit(final long secret) {
+		return (int) (secret - secret / 10 * 10);
+	}
+
+	@Override
+	public void aTest() {
+		assertEquals(6, new Part2().setIt(10).compute("test3.txt").res);
+		assertEquals(23, new Part2().compute("test2.txt").res);
+	}
+	
+	@Override
+	public void main() {
+		assertEquals(0, new Part2().compute("input.txt").res);
+	}
+	
+}
