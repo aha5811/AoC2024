0f2d6fb13520b55494248b3c254b0c98b37f8fa2
aha5811 <aha5811@users.noreply.github.com>
2025-01-23 12:56:48 +0100

    day 19.2 with memoization

--- aha-aoc2024/src/aha/aoc2024/TestAll.java
+++ aha-aoc2024/src/aha/aoc2024/TestAll.java
@@ -24,7 +24,7 @@ import org.junit.platform.suite.api.Suite;
 	aha.aoc2024.day16.Part1.class, aha.aoc2024.day16.Part2.class,
 	aha.aoc2024.day17.Part1.class, aha.aoc2024.day17.Part2.class,
 	aha.aoc2024.day18.Part1.class, /* aha.aoc2024.day18.Part2.class, */ aha.aoc2024.day18.Part2v2.class,
-	aha.aoc2024.day19.Part1.class, // aha.aoc2024.day19.Part2.class,
+	aha.aoc2024.day19.Part1.class, aha.aoc2024.day19.Part2.class,
 	aha.aoc2024.day20.Part1.class, aha.aoc2024.day20.Part2.class,
 	// aha.aoc2024.day21.Part1.class, aha.aoc2024.day21.Part2.class,
 	aha.aoc2024.day22.Part1.class, aha.aoc2024.day22.Part2.class,
--- aha-aoc2024/src/aha/aoc2024/day19/Part2.java
+++ aha-aoc2024/src/aha/aoc2024/day19/Part2.java
@@ -10,83 +10,47 @@ import java.util.Map;
 import aha.aoc2024.Part;
 
 public class Part2 extends Part1 {
-
-	// https://adventofcode.com/2024/day/19#part2
 	
+	// https://adventofcode.com/2024/day/19#part2
+
 	@Override
 	public Part compute(final String file) {
-		
+
 		final List<String> designs = new LinkedList<>();
-		List<String> patterns = new LinkedList<>();
+		final List<String> patterns = new LinkedList<>();
 		read(file, designs, patterns);
-
-		// these are the ones that were removed by compactation
-		final List<String> uncompactable = new LinkedList<>(patterns);
-
-		// idea: get all possible combinations by searching with trace
-		// for each combination c and for all uncompactable patterns ucp
-		//   if c contains ucp then
-		//     multiply res with number of combinations for ucp
-
-		patterns = compact(patterns);
 		
-		// without compacting (i.e. total search) the test case runs fine
-		// but for input it fails after 20min with out of memory with Xmx48g
-
-		uncompactable.removeAll(patterns);
-
-		// find all solutions for uncompactable with (remaining) patterns
-		final Map<String, List<List<String>>> u2 = new HashMap<>();
-		for (final String d : uncompactable) {
-			u2.put(d, new LinkedList<>());
-			doSearch(u2.get(d), new LinkedList<>(), d, patterns);
-		}
-
-		final List<List<String>> res = new LinkedList<>();
+		final Map<String, Long> s2combos = new HashMap<>();
+		s2combos.put("", 1l);
+		
 		for (final String d : designs)
-			doSearch(res, new LinkedList<>(), d, patterns);
-
-		for (final List<String> combination : res) {
-			System.out.println(combination);
-
-			// {rb=[[r, b]], gb=[[g, b]], br=[[b, r]]}
-			// [r, r, b, g, b, r]
-			// should be 6
-
-			// r b does not overlap so 2 * rest
-			// g b and b r do overlap with 3 possibilities (g b r, gb r, g br) so 3 * rest
-			// (1 *) 2 * 3 = 6
-
-			this.res += 1;
-		}
-
+			this.res += cntPossible(s2combos, d, patterns);
+		
 		return this;
 	}
+	
+	private long cntPossible(final Map<String, Long> s2combos, final String s, final List<String> patterns) {
+		if (s2combos.containsKey(s))
+			return s2combos.get(s);
 
-	private void doSearch(final List<List<String>> res, final List<String> dRes, final String d, final List<String> patterns) {
-		if (d.isEmpty())
-			res.add(dRes);
-		if (patterns.isEmpty())
-			return;
+		final List<String> sps = getPs4s(s, patterns);
 		
-		final List<String> dps = getPs4s(d, patterns);
-		
-		for (final String p : dps)
-			if (d.length() >= p.length() && d.startsWith(p)) {
-				final List<String> dResNext = new LinkedList<>(dRes);
-				dResNext.add(p);
-				doSearch(res, dResNext, d.substring(p.length()), dps);
-			}
+		long cnt = 0;
+		for (final String p : patterns)
+			if (s.length() >= p.length() && s.startsWith(p))
+				cnt += cntPossible(s2combos, s.substring(p.length()), sps);
+		s2combos.put(s, cnt);
+		return cnt;
 	}
-
+	
 	@Override
 	public void aTest() {
 		assertEquals(16, new Part2().compute("test.txt").res);
 	}
-	
+
 	@Override
 	public void main() {
-		// assertEquals(0, new Part2().compute("input.txt").res);
+		assertEquals(730121486795169l, new Part2().compute("input.txt").res);
 	}
-	
+
 }
